61c9b89b0ba4b022186cf1d8a30d420b40ccce42
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-04-10 15:38:55 +0200

    satisfy pylint

--- youtube_to_anki/main.py
+++ youtube_to_anki/main.py
@@ -1,3 +1,7 @@
+"""
+This module contains the main function for converting YouTube videos to Anki decks.
+"""
+
 from typing import Dict, Iterable
 
 import click
@@ -5,12 +9,16 @@ from pydub import AudioSegment
 
 from youtube_to_anki.anki import make_package as _make_package
 from youtube_to_anki.utils import process_audio_chunk, process_transcript_chunk
-from youtube_to_anki.youtube import retrieve_audio, retrieve_info, retrieve_transcript
+from youtube_to_anki.youtube import (retrieve_audio, retrieve_info,
+                                     retrieve_transcript)
 
 
 def make_package(
     transcript: Iterable[Dict], audio: AudioSegment, deck_name: str, filepath: str
 ):
+    """
+    Creates an Anki package from audio and transcript.
+    """
     transcript_chunks = tuple(process_transcript_chunk(chunk) for chunk in transcript)
     audio_chunks = tuple(
         process_audio_chunk(audio, chunk) for chunk in transcript_chunks
@@ -20,6 +28,7 @@ def make_package(
     )
 
 
+# pylint: disable=no-value-for-parameter
 @click.command()
 @click.option(
     "--out",
@@ -37,6 +46,9 @@ def main(
     transcript_language: str,
     out: str,
 ):
+    """
+    Converts a YouTube video into an Anki deck.
+    """
     url = f"https://www.youtube.com/watch?v={video_id}"
     transcript = retrieve_transcript(video_id, transcript_language)
     audio = retrieve_audio(url)
